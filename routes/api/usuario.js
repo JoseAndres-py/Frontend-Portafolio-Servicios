b85145a01681e83af018ff6e7637c593f607ccdc
JoseAndres-py <josean.lopezm@gmail.com>
2020-12-19 19:52:41 -0500

    Agregar modificaciones requerimientos usuarios

--- routes/api/usuario.js
+++ routes/api/usuario.js
@@ -4,10 +4,10 @@ const userController = require('../../controllers/UsuarioController.js');
 const router = routerx();
 
 // Route Api - User 
-router.get('/list', /*auth.verificarVendedor,*/ userController.list);
+router.get('/list', userController.list);
 
 // Route Api - User - add 
-router.post('/add', /*auth.verificarAdministrador,*/ userController.add);
+router.post('/add', userController.add);
 
 // Route Api - User - update 
 router.put('/update', userController.update);
